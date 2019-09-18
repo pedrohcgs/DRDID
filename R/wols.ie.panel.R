93916930527f80d762776f57fd1d9a950073e7e3
pedrohcgs <pedrohcgs@gmail.com>
2019-09-18 08:03:57 -0500

    internal function don't need to be roxygenize

--- R/wols.ie.panel.R
+++ R/wols.ie.panel.R
@@ -1,27 +1,6 @@
-#' @import stats
-NULL
 
 ###################################################################################
-# Compute Weighted OLS regression parameters for the (Intrinsically-Efficient) Doubly-Robust DID estimator with Panel Data
-#' Weighted OLS for Intrinsically Efficient DR-DID
-#'
-#' Compute Weighted OLS regression parameters for the (Intrinsically Efficient) Doubly-Robust DID estimator
-#'
-#' This function implements the Weighted OLS estimator for the Not-Treated group that is used to construct the
-#' (Intrinsically Efficient) Doubly Robust DID estimator, when Panel Data is available
-#'
-#' @param deltaY An \eqn{n} x \eqn{1} vector of the difference of outcomes between post and pre treatment periods, i.e., Y_post - Y_pre.
-#' @param D An \eqn{n} x \eqn{1} vector of Group indicators (=1 if observation is treated in the post-treatment, =0 otherwise).
-#' @param int.cov An \eqn{n} x \eqn{k} matrix of covariates to be used in the regression estimation (with intercept)
-#' @param pscore An \eqn{n} x \eqn{1} vector of propensity score estimates.
-#' @param i.weights An \eqn{n} x \eqn{1} vector of weights to be used.
-#'
-#' @return   A list containing the following components:
-#' \item{out.reg}{The fitted values of the outcome regression}
-#'
-#' @references
-#'
-#' Sant'Anna, Pedro H.C., and Zhao, Jun B. (2019) "Doubly Robust difference-in-difference estimators", \emph{Working Paper}.
+# Weighted OLS for Intrinsically Efficient DR-DID
 
 
 wols.ie.panel <- function(deltaY, D, int.cov, pscore, i.weights){
