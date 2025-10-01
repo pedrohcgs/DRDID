9399e8244baba01c16ee457c9cfa1ac47818cf5c
Pedro H. C. Sant'Anna <pedrohcgs@users.noreply.github.com>
2025-10-01 00:28:46 -0400

    Fix calculation of eta.treat and eta.cont

--- R/ipw_did_panel.R
+++ R/ipw_did_panel.R
@@ -122,8 +122,8 @@ ipw_did_panel <-function(y1, y0, D, covariates, i.weights = NULL,
   att.treat <- w.treat * deltaY
   att.cont <- w.cont * deltaY
 
-  eta.treat <- mean(att.treat) / mean(trim.level* i.weights * D)
-  eta.cont <- mean(att.cont) / mean(trim.level* i.weights * D)
+  eta.treat <- mean(att.treat) / mean(i.weights * D)
+  eta.cont <- mean(att.cont) / mean(i.weights * D)
 
   ipw.att <- eta.treat - eta.cont
   #-----------------------------------------------------------------------------
