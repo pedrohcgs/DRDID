37e47e7048c39f5804ef184a07dede3a6e4b148b
Pedro H. C. Sant'Anna <pedrohcgs@users.noreply.github.com>
2025-10-02 13:31:10 -0400

    Update reg_did_rc.R
    Adjust standard error calculation for regression adjustment.

--- R/reg_did_rc.R
+++ R/reg_did_rc.R
@@ -189,7 +189,7 @@ reg_did_rc <-function(y, post, D, covariates, i.weights = NULL,
   #-----------------------------------------------------------------------------
   if (boot == FALSE) {
     # Estimate of standard error
-    se.reg.att <- stats::sd(reg.att.inf.func)/sqrt(n)
+    se.reg.att <- stats::sd(reg.att.inf.func)*sqrt(n-1)/(n)
     # Estimate of upper boudary of 95% CI
     uci <- reg.att + 1.96 * se.reg.att
     # Estimate of lower doundary of 95% CI
