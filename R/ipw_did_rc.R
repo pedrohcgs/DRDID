807973478e38bd79507a4f7ff5d250a1c1953153
Pedro H. C. Sant'Anna <pedrohcgs@users.noreply.github.com>
2025-10-02 13:29:55 -0400

    Update ipw_did_rc.R
    Adjust standard error calculation for ATT estimation.

--- R/ipw_did_rc.R
+++ R/ipw_did_rc.R
@@ -179,7 +179,7 @@ ipw_did_rc <-function(y, post, D, covariates, i.weights = NULL,
   #-----------------------------------------------------------------------------
   if (boot == FALSE) {
     # Estimate of standard error
-    se.att <- stats::sd(att.inf.func)/sqrt(n)
+    se.att <- stats::sd(att.inf.func)*sqrt(n-1)/(n)
     # Estimate of upper boudary of 95% CI
     uci <- ipw.att + 1.96 * se.att
     # Estimate of lower doundary of 95% CI
